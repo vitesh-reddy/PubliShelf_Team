8398048f9a08296d5d39d6f4a437b4e20321f008
vitesh-reddy <k.l.viteshreddy@gmail.com>
2025-11-30 17:26:39 +0530

    feat: update auction image scaling effect on hover

--- client/src/pages/buyer/dashboard/components/AuctionCountdownCard.jsx
+++ client/src/pages/buyer/dashboard/components/AuctionCountdownCard.jsx
@@ -38,7 +38,7 @@ const AuctionCountdownCard = ({ auction, onClick }) => {
             <img
               src={auction.image}
               alt={auction.title}
-              className="w-full h-full object-cover group-hover:scale-110 transition-transform duration-300"
+              className="w-full h-full object-cover group-hover:scale-105 transition-transform duration-300"
             />
           </div>
         </div>
