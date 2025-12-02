b303be721038ecc12f47b4bd66687f038099c6ed
vitesh-reddy <k.l.viteshreddy@gmail.com>
2025-12-02 22:39:56 +0530

    Add NotFound page

--- client/src/routes/PublicRoutes.jsx
+++ client/src/routes/PublicRoutes.jsx
@@ -4,10 +4,12 @@ import Home from '../pages/public/Home/Index';
 import About from '../pages/public/About';
 import Contact from '../pages/public/Contact';
 import AdminDashboard from '../pages/admin/dashboard/Dashboard';
+import NotFound from '../pages/public/NotFound';
 
 const PublicRoutes = () => (
   <>
     <Route path="/" element={<Home />} />
+    <Route path="*" element={<NotFound />} />
     <Route path="/about" element={<About />} />
     <Route path="/contact" element={<Contact />} />
     <Route path="/admin/dashboard/:key" element={<AdminDashboard />} />
